5338fde640eee7235ce4d73e8604c5bf4baf6791
Bryan Berger <bryan.berger@netsteel.space>
2019-11-08 14:48:40 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoList';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBJ8EcKJYAZpOtIkvydMOuyFbJO6InrZKY',
+      authDomain: 'todolist-362d9.firebaseapp.com',
+      databaseURL: 'https://todolist-362d9.firebaseio.com',
+      projectId: 'todolist-362d9',
+      storageBucket: 'todolist-362d9.appspot.com',
+      messagingSenderId: '131184828891',
+      appId: '1:131184828891:web:114e0ffab0bc78940147fe'
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
